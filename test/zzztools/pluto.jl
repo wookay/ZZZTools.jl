9a4cb45eeb5654460b9d927b9e7b977719487e41
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-03 10:29:37 +0900

    using Jive v0.3.2 sprint_html

--- test/zzztools/pluto.jl
+++ test/zzztools/pluto.jl
@@ -14,9 +14,6 @@ big = GO.get_wengines_big("LunarPleniluna", "[Lunar] Pleniluna")
 @test big isa Resource
 
 anby = Character(Id = 1011)
-
-if isdefined(Jive, :sprint_html)
 @test startswith(sprint_html(anby), "<div")
-end # if sprint_html
 
 end # module test_zzztools_pluto
